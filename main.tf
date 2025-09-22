730ae9cfe0e639465f5bb9145cd615e604d7e06d
vinay <vinaybkrishna10@gmail.com>
2025-09-22 18:14:49 +0530

    terraform instace creation

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.81.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "my_instance" {
+  ami = "ami-09c813fb71547fc4f"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "EC2-1"
+  }
+}
