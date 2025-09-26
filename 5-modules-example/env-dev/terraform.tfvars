8560c41d61ced81f0bc37f69e4b9a1eb0d2cb0d5
vinay <vinaybkrishna10@gmail.com>
2025-09-26 16:41:15 +0530

    terraform

--- 5-modules-example/env-dev/terraform.tfvars
+++ 5-modules-example/env-dev/terraform.tfvars
@@ -1,3 +1,3 @@
 instance_type = "t3.micro"
 
-aws_region = "us-east-1"
+aws_region = "us-east-2"
--- 5-modules-example/provider.tf
+++ 5-modules-example/provider.tf
@@ -5,19 +5,10 @@ provider "aws" {
 
 
 terraform {
-  required_version = ">= 1.5, < 2.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.60, < 6.0"
-    }
-  }
-
   backend "s3" {
-    bucket         = "learning-bucket"    # existing bucket
+    bucket         = "learning-bucket-307"    # existing bucket
     key            = "terraform-module/test/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     encrypt        = true
     use_lockfile   = true
   }
