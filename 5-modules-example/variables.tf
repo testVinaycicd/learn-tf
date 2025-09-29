14f55bc2166e6bdbbfdc6357b21b242aff5821cd
vinay <vinaybkrishna10@gmail.com>
2025-09-29 16:14:03 +0530

    terraform

--- 5-modules-example/variables.tf
+++ 5-modules-example/variables.tf
@@ -2,8 +2,14 @@ variable "aws_region" { type = string }
 variable "instance_type" { type = string }
 
 
-variable "vpc_cidr" { type = string, default = "10.0.0.0/16" }
-variable "azs" { type = list(string), default = ["us-east-2a","us-east-2b"] }
+variable "vpc_cidr" {
+  type = string
+  default = "10.0.0.0/16"
+}
+variable "azs" {
+  type = list(string)
+  default = ["us-east-2a","us-east-2b"]
+}
 
 variable "name" {}
 variable "subnet_id" {}
