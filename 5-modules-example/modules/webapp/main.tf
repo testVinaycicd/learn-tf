f009cfffd109732cfa06b470fe87c4f12886de29
vinay <vinaybkrishna10@gmail.com>
2025-09-29 16:08:24 +0530

    terraform

--- 5-modules-example/modules/webapp/main.tf
+++ 5-modules-example/modules/webapp/main.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "ec2_example" {
   ami = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
   subnet_id = var.subnet_id
-  vpc_security_group_ids = [aws_security_group.main.id]
+  vpc_security_group_ids = [var.security_group]
   iam_instance_profile = var.iam_instance_profile
 
   tags = {
@@ -33,31 +33,7 @@ resource "aws_instance" "ec2_example" {
       EOF
 }
 
-resource "aws_security_group" "main" {
-  name        = "EC2-webserver-SG-1"
-  description = "Webserver for EC2 Instances"
 
-  ingress {
-    from_port   = 80
-    protocol    = "TCP"
-    to_port     = 80
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    protocol    = "TCP"
-    to_port     = 22
-    cidr_blocks = ["115.97.103.44/32"]
-  }
-
-  egress {
-    from_port   = 0
-    protocol    = "-1"
-    to_port     = 0
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
 
 output "public_ip" {
   value = aws_instance.ec2_example.public_ip
--- 5-modules-example/modules/webapp/variables.tf
+++ 5-modules-example/modules/webapp/variables.tf
@@ -1,7 +1,6 @@
 variable "name" { type = string, default = "demo" }
-variable "ami" { type = string, default = "" }    # set to an AMI for your region or use data source
-variable "instance_type" { type = string, default = "t3.micro" }
-variable "user_data" { type = string, default = "" }
+variable "instance_type" { }
 
 variable "subnet_id" {}
 variable "iam_instance_profile" {}
+variable "security_group" {}
