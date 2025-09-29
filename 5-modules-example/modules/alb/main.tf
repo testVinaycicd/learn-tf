55ee2b6f7a1871cad32d13f07c64a2d52b6c5d46
vinay <vinaybkrishna10@gmail.com>
2025-09-30 01:27:42 +0530

    terraform

--- 5-modules-example/modules/alb/main.tf
+++ 5-modules-example/modules/alb/main.tf
@@ -60,14 +60,17 @@ resource "aws_lb_target_group_attachment" "tga" {
 
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
-  port              = "80"
+  port              = 80
   protocol          = "HTTP"
   default_action {
-    type = "forward"
-    target_group_arn = aws_lb_target_group.tg.arn
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
-
 # ALB listens on port 80.
 # Forwards traffic to the Target Group.
 # Target Group distributes traffic to all attached EC2 instances (on port 80).
