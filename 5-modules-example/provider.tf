892dc731b703973f3a2304a8fc1cbba1ae88d50e
vinay <vinaybkrishna10@gmail.com>
2025-09-29 16:16:02 +0530

    terraform

--- 5-modules-example/provider.tf
+++ 5-modules-example/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.81.0, <= 5.100.0"   # or "5.100.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.aws_region
 }
