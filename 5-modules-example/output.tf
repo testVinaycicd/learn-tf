79d790c5c86bc1a40241583b99c0aad0cb124b48
vinay <vinaybkrishna10@gmail.com>
2025-10-04 19:19:51 +0530

    terraform

--- 5-modules-example/output.tf
+++ 5-modules-example/output.tf
@@ -1,7 +1,7 @@
-output "check-webserver" {
-  value = module.web_server_1.public_ip
-}
-
-output "alb_dns" {
-  value = module.alb.alb_dns_name
-}
+# output "check-webserver" {
+#   value = module.web_server_1.public_ip
+# }
+#
+# output "alb_dns" {
+#   value = module.alb.alb_dns_name
+# }
