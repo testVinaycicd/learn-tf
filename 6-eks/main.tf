97b3e5b9574f891a77dcc31f4832e9d99e3189bf
vinay <vinaybkrishna10@gmail.com>
2025-10-04 20:00:55 +0530

    terraform

--- 6-eks/main.tf
+++ 6-eks/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "5.81.0"
     }
   }
+  backend "s3" {
+    bucket = "learning-bucket-307"    # existing bucket
+    key          = "terraform-module/test/terraform-eks.tfstate"
+    region       = "us-east-2"
+    encrypt      = true
+    use_lockfile = true
+  }
 }
 provider "aws" {
   region = "us-east-2"
