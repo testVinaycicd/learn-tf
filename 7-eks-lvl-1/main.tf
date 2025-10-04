2a59ff043b36a54b28608d1727cae41512c8d557
vinay <vinaybkrishna10@gmail.com>
2025-10-05 02:21:14 +0530

    terraform

--- 7-eks-lvl-1/main.tf
+++ 7-eks-lvl-1/main.tf
@@ -179,17 +179,3 @@ resource "aws_security_group_rule" "nodes_to_api" {
   source_security_group_id = aws_security_group.nodes.id
 }
 
-variable "bastion_sg_id" {
-  type        = string
-  description = "Security group ID of the bastion/ops host that runs kubectl"
-}
-
-resource "aws_security_group_rule" "bastion_to_api" {
-  type                     = "ingress"
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  security_group_id        = aws_eks_cluster.this.vpc_config[0].cluster_security_group_id
-  source_security_group_id = var.bastion_sg_id
-  description              = "Bastion to EKS API server"
-}
