57a20b9c110b6149756b86780a7234843e857652
vinay <vinaybkrishna10@gmail.com>
2025-10-06 22:28:08 +0530

    terraform

--- 8-eks-test/main.tf
+++ 8-eks-test/main.tf
@@ -100,7 +100,7 @@ resource "null_resource" "server_setup" {
       "sudo pip3 install ansible hvac",
 
       " sleep 5",
-      "ansible-playbook -i localhost, setup.yaml  -e tool_name=${each.value.tags.Name} "
+      "ansible-pull  -U https://github.com/testVinaycicd/learn-tf.git -C main  -i localhost, 8-eks-test/setup.yaml  -e \"tool_name=${each.value.tags.Name}\" "
     ]
 
 
