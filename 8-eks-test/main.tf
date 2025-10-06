8cf05127bf6c2dd17c5964913867ff01834b4573
vinay <vinaybkrishna10@gmail.com>
2025-10-06 21:45:32 +0530

    terraform

--- 8-eks-test/main.tf
+++ 8-eks-test/main.tf
@@ -30,6 +30,18 @@ resource "local_file" "pem_file" {
 
 }
 
+resource "aws_s3_object" "pem_to_s3" {
+  bucket = "learning-bucket-307"                      # existing bucket
+  key    = "keys/mikey-auto-key.pem"             # or add a timestamp if you want rotation
+  content = tls_private_key.generated.private_key_pem
+
+
+  tags = {
+    Purpose = "EC2 SSH private key"
+    Owner   = "Mikey"
+  }
+}
+
 # --- Get the latest Amazon Linux 2 AMI ---
 data "aws_ami" "amazon_linux" {
   most_recent = true
@@ -52,5 +64,28 @@ resource "aws_instance" "servers" {
   tags = {
     Name        = "mikey-server-${count.index + 1}"
     Environment = "dev"
+  }
+}
+
+resource "null_resource" "server_setup" {
+  depends_on = [aws_instance.servers]
+
+  triggers = {
+    instance_id_change = aws_instance.servers.id
+  }
+
+  provisioner "remote-exec" {
+
+    inline = [
+      "aws s3 cp s3://learning-bucket-307/keys/mikey-auto-key.pem .",
+      "chmod 600 mikey-auto-key.pem",
+      "PUBLIC_IP=$(curl -s http://checkip.amazonaws.com)",
+      "ssh -i mikey-auto-key.pem ec2-user@PUBLIC_IP",
+      "sudo pip3.11 install ansible hvac",
+      " do echo 'Waiting for DNS...'; sleep 5; done",
+      "ansible-playbook -i localhost, setup-tool.yml  -e tool_name=instance "
+    ]
+
+
   }
 }
