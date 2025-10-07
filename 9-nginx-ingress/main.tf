f1cecfde4b908b34c1750da4c7b6805dc8ce3d07
vinay <vinaybkrishna10@gmail.com>
2025-10-08 00:22:13 +0530

    terraform

--- 9-nginx-ingress/main.tf
+++ 9-nginx-ingress/main.tf
@@ -7,8 +7,8 @@ module "vpc" {
 
 module "eks" {
   source = "./modules/eks"
-  private_subnet_ids = [module.vpc.private_subnet_ids]
-  vpc_id = [module.vpc.vpc_id]
+  private_subnet_ids = module.vpc.private_subnet_ids
+  vpc_id = module.vpc.vpc_id
   access = var.access
   region = var.aws_region
 }
