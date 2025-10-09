d1a842098b60a01ca4d9b1d648612559c7f4646d
vinay <vinaybkrishna10@gmail.com>
2025-10-09 19:19:47 +0530

    terraform

--- 9-nginx-ingress/variable.tf
+++ 9-nginx-ingress/variable.tf
@@ -1,16 +1,15 @@
 variable "aws_region" { type = string }
 
-
 variable "vpc_cidr" {
   type = string
   default = "10.0.0.0/16"
 }
+
 variable "azs" {
   type = list(string)
   default = ["us-east-1a","us-east-1b"]
 }
 
 variable "name" {}
+
 variable "access" {}
-variable "instance_type" {}
-variable "private_rt_ids" {}
