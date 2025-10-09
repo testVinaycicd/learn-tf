acf09b82a8a9a2ae64f07a8252c2171ad3fb49ae
vinay <vinaybkrishna10@gmail.com>
2025-10-10 02:04:51 +0530

    terraform

--- 9-nginx-ingress/main.tf
+++ 9-nginx-ingress/main.tf
@@ -16,26 +16,11 @@ module "eks" {
   private_rt_ids = module.vpc.private_route_table_ids
 }
 
-provider "kubernetes" {
-  config_context = module.eks.cluster_name
-  config_path = pathexpand("~/.kube/config")
-
-}
-
-provider "helm" {
-  kubernetes {
-    config_path = pathexpand("~/.kube/config")
-    config_context = module.eks.cluster_name
-  }
-}
 
 
 
 module "addons" {
   source     = "./modules/addons"
   depends_on = [module.eks]      # ensures cluster exists before planning this module
-  providers = {
-    kubernetes = kubernetes
-    helm       = helm
-  }
+
 }
--- 9-nginx-ingress/provider.tf
+++ 9-nginx-ingress/provider.tf
@@ -28,6 +28,16 @@ terraform {
 }
 
 
+provider "kubernetes" {
+  config_path = pathexpand("~/.kube/config")
+
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = pathexpand("~/.kube/config")
+  }
+}
 
 provider "aws" {
   region = var.aws_region
