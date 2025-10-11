22a9a70355b2be62014d540090574f6e07648d5c
vinay <vinaybkrishna10@gmail.com>
2025-10-11 18:24:26 +0530

    terraform

--- 9-nginx-ingress/main.tf
+++ 9-nginx-ingress/main.tf
@@ -7,20 +7,20 @@ module "vpc" {
   azs = var.azs
 }
 
-# module "eks" {
-#   source = "./modules/eks"
-#   private_subnet_ids = module.vpc.private_subnet_ids
-#   vpc_id = module.vpc.vpc_id
-#   access = var.access
-#   region = var.aws_region
-#   private_rt_ids = module.vpc.private_route_table_ids
-# }
-#
-#
-#
-#
-# module "addons" {
-#   source     = "./modules/addons"
-#   depends_on = [module.eks]      # ensures cluster exists before planning this module
-#
-# }
+module "eks" {
+  source = "./modules/eks"
+  private_subnet_ids = module.vpc.private_subnet_ids
+  vpc_id = module.vpc.vpc_id
+  access = var.access
+  region = var.aws_region
+  private_rt_ids = module.vpc.private_route_table_ids
+}
+
+
+
+
+module "addons" {
+  source     = "./modules/addons"
+  depends_on = [module.eks]      # ensures cluster exists before planning this module
+
+}
