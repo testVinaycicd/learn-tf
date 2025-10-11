4aed4ef477cc84d538a41cb64ceed93a16795467
vinay <vinaybkrishna10@gmail.com>
2025-10-11 18:58:02 +0530

    terraform

--- 9-nginx-ingress/modules/addons/helm.tf
+++ 9-nginx-ingress/modules/addons/helm.tf
@@ -15,10 +15,14 @@ resource "null_resource" "tesd-config" {
 }
 
 resource "kubernetes_namespace" "ingress" {
+  depends_on = [null_resource.kubeconfig,null_resource.tesd-config,kubernetes_namespace.ingress]
+
   metadata { name = "ingress-nginx" }
 }
 
 resource "kubernetes_namespace" "cert_manager" {
+  depends_on = [null_resource.kubeconfig,null_resource.tesd-config,kubernetes_namespace.ingress]
+
   metadata { name = "cert-manager" }
 }
 
@@ -27,7 +31,7 @@ resource "helm_release" "ingress" {
   name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
-  namespace  = "ingress-nginx"
+  namespace  = kubernetes_namespace.ingress.metadata[0].name
   values = [
     file("${path.module }/helmconfig/ingress.yaml")
   ]
@@ -43,13 +47,13 @@ resource "helm_release" "cert-manager" {
   name             = kubernetes_namespace.cert_manager.metadata[0].name
   repository       = "https://charts.jetstack.io"
   chart            = "cert-manager"
-  namespace        = "cert-manager"
+  namespace        = kubernetes_namespace.ingress.metadata[0].name
 
 
-  set_sensitive = [ {
+  set {
     name  = "installCRDs"
-    value = "false"
-  }]
+    value = "true"
+  }
 
   wait    = true
   timeout = 600
@@ -67,16 +71,23 @@ resource "null_resource" "cert-manager-cluster-issuer" {
 }
 
 resource "helm_release" "external-dns" {
-  depends_on = [null_resource.kubeconfig]
+  depends_on = [null_resource.kubeconfig,helm_release.ingress]
   name       = "external-dns"
   repository = "https://kubernetes-sigs.github.io/external-dns/"
   chart      = "external-dns"
+  namespace  = "kube-system"
+
+  wait    = true
+  timeout = 600
+
 }
 
 resource "null_resource" "nginx_issuer" {
-  depends_on = [null_resource.kubeconfig, helm_release.cert-manager,null_resource.cert-manager-cluster-issuer,helm_release.external-dns]
+  depends_on = [null_resource.kubeconfig, helm_release.cert-manager,null_resource.cert-manager-cluster-issuer,helm_release.external-dns,helm_release.ingress]
 
   provisioner "local-exec" {
     command = "kubectl apply -f ${path.module}/helmconfig/nginx-ingress-setup.yml"
   }
+
+
 }
