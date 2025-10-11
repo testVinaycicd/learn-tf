9f16c6803bb7ce6edbb60a35c867ab963623689b
vinay <vinaybkrishna10@gmail.com>
2025-10-11 16:33:28 +0530

    terraform

--- 9-nginx-ingress/modules/addons/helm.tf
+++ 9-nginx-ingress/modules/addons/helm.tf
@@ -13,51 +13,51 @@ resource "null_resource" "tesd-config" {
     command = "kubectl get nodes "
   }
 }
-
-resource "helm_release" "ingress" {
-  depends_on = [null_resource.kubeconfig]
-  name       = "ingress-nginx-tf-1"
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-
-  values = [
-    file("${path.module }/helmconfig/ingress.yaml")
-  ]
-}
-
-resource "helm_release" "cert-manager" {
-  depends_on       = [null_resource.kubeconfig]
-  name             = "cert-manager-tf-1"
-  repository       = "https://charts.jetstack.io"
-  chart            = "cert-manager"
-  namespace        = "cert-manager"
-  create_namespace = true
-
-  set_sensitive = [ {
-    name  = "crds.enabled"
-    value = "true"
-  }]
-}
-
-resource "null_resource" "cert-manager-cluster-issuer" {
-  depends_on = [null_resource.kubeconfig, helm_release.cert-manager]
-
-  provisioner "local-exec" {
-    command = "kubectl apply -f ${path.module}/helmconfig/cluster-issuer.yaml"
-  }
-}
-
-resource "helm_release" "external-dns" {
-  depends_on = [null_resource.kubeconfig]
-  name       = "external-dns-tf-1"
-  repository = "https://kubernetes-sigs.github.io/external-dns/"
-  chart      = "external-dns"
-}
-
-resource "null_resource" "nginx_issuer" {
-  depends_on = [null_resource.kubeconfig, helm_release.cert-manager,null_resource.cert-manager-cluster-issuer,helm_release.external-dns]
-
-  provisioner "local-exec" {
-    command = "kubectl apply -f ${path.module}/helmconfig/nginx-ingress-setup.yaml"
-  }
-}
+#
+# resource "helm_release" "ingress" {
+#   depends_on = [null_resource.kubeconfig]
+#   name       = "ingress-nginx-tf-1"
+#   repository = "https://kubernetes.github.io/ingress-nginx"
+#   chart      = "ingress-nginx"
+#
+#   values = [
+#     file("${path.module }/helmconfig/ingress.yaml")
+#   ]
+# }
+#
+# resource "helm_release" "cert-manager" {
+#   depends_on       = [null_resource.kubeconfig]
+#   name             = "cert-manager-tf-1"
+#   repository       = "https://charts.jetstack.io"
+#   chart            = "cert-manager"
+#   namespace        = "cert-manager"
+#   create_namespace = true
+#
+#   set_sensitive = [ {
+#     name  = "crds.enabled"
+#     value = "true"
+#   }]
+# }
+#
+# resource "null_resource" "cert-manager-cluster-issuer" {
+#   depends_on = [null_resource.kubeconfig, helm_release.cert-manager]
+#
+#   provisioner "local-exec" {
+#     command = "kubectl apply -f ${path.module}/helmconfig/cluster-issuer.yaml"
+#   }
+# }
+#
+# resource "helm_release" "external-dns" {
+#   depends_on = [null_resource.kubeconfig]
+#   name       = "external-dns-tf-1"
+#   repository = "https://kubernetes-sigs.github.io/external-dns/"
+#   chart      = "external-dns"
+# }
+#
+# resource "null_resource" "nginx_issuer" {
+#   depends_on = [null_resource.kubeconfig, helm_release.cert-manager,null_resource.cert-manager-cluster-issuer,helm_release.external-dns]
+#
+#   provisioner "local-exec" {
+#     command = "kubectl apply -f ${path.module}/helmconfig/nginx-ingress-setup.yaml"
+#   }
+# }
