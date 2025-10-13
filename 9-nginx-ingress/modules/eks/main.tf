b4b78fa3618fffd2c0b291362eea060175dce1a9
vinay <vinaybkrishna10@gmail.com>
2025-10-13 23:17:13 +0530

    terraform

--- 9-nginx-ingress/modules/eks/main.tf
+++ 9-nginx-ingress/modules/eks/main.tf
@@ -44,7 +44,7 @@ resource "aws_eks_cluster" "this" {
     subnet_ids               = var.private_subnet_ids
     endpoint_private_access  = true
     endpoint_public_access   = true
-    public_access_cidrs      = ["34.204.49.219/32"]
+    public_access_cidrs      = ["107.23.27.81/32"]
   }
 
 
