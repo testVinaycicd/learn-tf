1923ad9c9b353950a0152b76e18fce62b9417864
vinay <vinaybkrishna10@gmail.com>
2025-10-13 20:08:40 +0530

    terraform

--- 11-ssm/modules/ssm_instance/main.tf
+++ 11-ssm/modules/ssm_instance/main.tf
@@ -129,7 +129,7 @@ data "aws_ssm_parameter" "al2023_ami" {
 }
 
 resource "aws_instance" "bastionless_priv" {
-  ami                         = data.aws_ssm_parameter.al2023_ami.value
+  ami                         = "ami-09c813fb71547fc4f"
   instance_type               = "t3.micro"
   subnet_id                   = var.private_subnet_id
   associate_public_ip_address = false
