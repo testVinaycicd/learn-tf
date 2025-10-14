f2688387f4de6316fcb25f3eead865b27011ab1c
vinay <vinaybkrishna10@gmail.com>
2025-10-14 11:01:34 +0530

    terraform

--- 9-nginx-ingress/modules/addons/helm.tf
+++ 9-nginx-ingress/modules/addons/helm.tf
@@ -14,17 +14,18 @@ resource "null_resource" "tesd-config" {
   }
 }
 
-# resource "kubernetes_namespace" "ingress" {
-#   depends_on = [null_resource.kubeconfig,null_resource.tesd-config]
-#
-#   metadata { name = "ingress-nginx" }
-# }
-#
-# resource "kubernetes_namespace" "cert_manager" {
-#   depends_on = [null_resource.kubeconfig,null_resource.tesd-config]
-#
-#   metadata { name = "cert-manager" }
-# }
+resource "kubernetes_namespace" "ingress" {
+  depends_on = [null_resource.kubeconfig,null_resource.tesd-config]
+
+  metadata { name = "ingress-nginx" }
+
+}
+
+resource "kubernetes_namespace" "cert_manager" {
+  depends_on = [null_resource.kubeconfig,null_resource.tesd-config]
+
+  metadata { name = "cert-manager" }
+}
 
 resource "helm_release" "ingress" {
   depends_on = [null_resource.kubeconfig,null_resource.tesd-config]
@@ -78,76 +79,76 @@ resource "null_resource" "cert-manager-cluster-issuer" {
 
 
 
-resource "helm_release" "external-dns" {
-  name       = "external-dns"
-  repository = "https://kubernetes-sigs.github.io/external-dns/"
-  chart      = "external-dns"
-  namespace  = "kube-system"
-
-  wait    = true
-  timeout = 600
-
-  values = [
-    yamlencode({
-      serviceAccount = {
-        create = true
-        name   = "external-dns"
-      }
-      provider     = "aws"
-      policy       = "upsert-only"
-      registry     = "txt"
-      txtOwnerId   = "mikey"
-      domainFilters = ["mikeydevops1.online"]
-      sources      = ["ingress", "service"]
-      aws = {
-        zoneType = "public"
-      }
-      logLevel  = "info"
-      interval  = "1m"
-    })
-  ]
-
-  depends_on = [
-
-    null_resource.kubeconfig,
-    helm_release.ingress
-  ]
-
-
-}
-
-
-
-
-# resource "null_resource" "wait_ingress_ready" {
-#   depends_on = [helm_release.ingress]
+# resource "helm_release" "external-dns" {
+#   name       = "external-dns"
+#   repository = "https://kubernetes-sigs.github.io/external-dns/"
+#   chart      = "external-dns"
+#   namespace  = "kube-system"
 #
-#   provisioner "local-exec" {
-#     command = <<-EOT
-#       set -euo pipefail
+#   wait    = true
+#   timeout = 600
 #
-#       # 1) Controller up
-#       kubectl -n ingress-nginx wait deploy/ingress-nginx-controller \
-#         --for=condition=Available=True --timeout=5m
+#   values = [
+#     yamlencode({
+#       serviceAccount = {
+#         create = true
+#         name   = "external-dns"
+#       }
+#       provider     = "aws"
+#       policy       = "upsert-only"
+#       registry     = "txt"
+#       txtOwnerId   = "mikey"
+#       domainFilters = ["mikeydevops1.online"]
+#       sources      = ["ingress", "service"]
+#       aws = {
+#         zoneType = "public"
+#       }
+#       logLevel  = "info"
+#       interval  = "1m"
+#     })
+#   ]
 #
-#       # 2) Admission jobs completed (names used by the official chart)
-#       kubectl -n ingress-nginx wait job/ingress-nginx-admission-create \
-#         --for=condition=complete --timeout=5m || true
-#       kubectl -n ingress-nginx wait job/ingress-nginx-admission-patch \
-#         --for=condition=complete --timeout=5m || true
+#   depends_on = [
 #
-#       # 3) Webhook object exists and has CABundle populated (best-effort check)
-#       kubectl get validatingwebhookconfiguration ingress-nginx-admission >/dev/null 2>&1 || true
-#     EOT
-#   }
+#     null_resource.kubeconfig,
+#     helm_release.ingress
+#   ]
+#
+#
 # }
 #
-resource "null_resource" "nginx_issuer" {
-  depends_on = [null_resource.kubeconfig, helm_release.cert-manager,null_resource.cert-manager-cluster-issuer,helm_release.external-dns,helm_release.ingress]
-
-  provisioner "local-exec" {
-    command = "kubectl apply -f ${path.module}/helmconfig/nginx-ingress-setup.yml"
-  }
-
-
-}
+#
+#
+#
+# # resource "null_resource" "wait_ingress_ready" {
+# #   depends_on = [helm_release.ingress]
+# #
+# #   provisioner "local-exec" {
+# #     command = <<-EOT
+# #       set -euo pipefail
+# #
+# #       # 1) Controller up
+# #       kubectl -n ingress-nginx wait deploy/ingress-nginx-controller \
+# #         --for=condition=Available=True --timeout=5m
+# #
+# #       # 2) Admission jobs completed (names used by the official chart)
+# #       kubectl -n ingress-nginx wait job/ingress-nginx-admission-create \
+# #         --for=condition=complete --timeout=5m || true
+# #       kubectl -n ingress-nginx wait job/ingress-nginx-admission-patch \
+# #         --for=condition=complete --timeout=5m || true
+# #
+# #       # 3) Webhook object exists and has CABundle populated (best-effort check)
+# #       kubectl get validatingwebhookconfiguration ingress-nginx-admission >/dev/null 2>&1 || true
+# #     EOT
+# #   }
+# # }
+# #
+# resource "null_resource" "nginx_issuer" {
+#   depends_on = [null_resource.kubeconfig, helm_release.cert-manager,null_resource.cert-manager-cluster-issuer,helm_release.external-dns,helm_release.ingress]
+#
+#   provisioner "local-exec" {
+#     command = "kubectl apply -f ${path.module}/helmconfig/nginx-ingress-setup.yml"
+#   }
+#
+#
+# }
