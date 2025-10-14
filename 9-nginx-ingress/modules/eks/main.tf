0f16c80b008b041e2d91bfbf7280daf585ec51bd
vinay <vinaybkrishna10@gmail.com>
2025-10-14 12:20:16 +0530

    terraform

--- 9-nginx-ingress/modules/eks/main.tf
+++ 9-nginx-ingress/modules/eks/main.tf
@@ -67,7 +67,7 @@ resource "aws_iam_role_policy_attachment" "external-dns-route53-full-access" {
 
 resource "aws_eks_pod_identity_association" "external-dns" {
   cluster_name    = aws_eks_cluster.this.name
-  namespace       = "default"
+  namespace       = "kube-system"
   service_account = "external-dns"
   role_arn        = aws_iam_role.external-dns.arn
 }
