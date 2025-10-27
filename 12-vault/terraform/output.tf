685b8ea6f582bedac6ad9715760d16b6fde540a2
vinay <vinaybkrishna10@gmail.com>
2025-10-27 14:46:38 +0530

    terraform

--- 12-vault/terraform/output.tf
+++ 12-vault/terraform/output.tf
@@ -1,5 +1,6 @@
 output "vault_url" {
-  value = "https://${var.domain_name}" description = "Public URL via ALB"
+  value = "https://${var.domain_name}"
+  description = "Public URL via ALB"
 }
 
 output "vault_instance_id" {
