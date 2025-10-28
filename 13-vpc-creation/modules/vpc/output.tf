58599282525b70c3449a5e8df826869e5c54c01d
vinay <vinaybkrishna10@gmail.com>
2025-10-28 15:19:49 +0530

    terraform

--- 13-vpc-creation/modules/vpc/output.tf
+++ 13-vpc-creation/modules/vpc/output.tf
@@ -34,39 +34,6 @@ output "public_route_table_id" {
   description = "Public route table ID"
 }
 
-# Private subnets (map keyed by AZ)
-output "private_subnet_ids" {
-  value       = [ for s in aws_subnet.private : s.id ]
-  description = "Private subnet IDs keyed by AZ"
-}
-
-output "private_subnet_cidrs" {
-  value       = { for az, s in aws_subnet.private : az => s.cidr_block }
-  description = "Private subnet CIDRs keyed by AZ"
-}
-
-# NAT resources (maps keyed by the AZ key used for public subnets)
-output "nat_eip_allocation_ids" {
-  value       = { for az, e in aws_eip.nat : az => e.id }
-  description = "NAT EIP allocation resource IDs keyed by AZ"
-}
-
-output "nat_eip_public_ips" {
-  value       = { for az, e in aws_eip.nat : az => e.public_ip }
-  description = "NAT EIP public IPs keyed by AZ"
-}
-
-output "nat_gateway_ids" {
-  value       = { for az, n in aws_nat_gateway.this : az => n.id }
-  description = "NAT Gateway IDs keyed by AZ"
-}
-
-# Private route tables (map keyed by AZ of the private subnet)
-output "private_route_table_ids" {
-  value       = { for az, rt in aws_route_table.private : az => rt.id }
-  description = "Private route table IDs keyed by AZ"
-}
-
 output "ec2_security_group_id" {
   value       = aws_security_group.ec2_sg.id
   description = "Security group ID for EC2"
