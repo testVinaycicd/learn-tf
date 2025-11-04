731f36c3208a92ef12bc58ea4a0b23cf54b3e64a
vinay <vinaybkrishna10@gmail.com>
2025-11-05 00:15:32 +0530

    terraform

---
+++ learn-helm/precona-mongodb/run.sh
@@ -0,0 +1,18 @@
+image_tag=$1
+
+PASSWORD=$(kubectl get secrets -n argocd argocd-initial-admin-secret -o json | jq .data.password|xargs | base64 --decode)
+argocd login learn-argocd-dev.mikeydevops1.online --grpc-web --insecure --username admin --password $PASSWORD
+
+
+# Operator
+argocd app create psmdb-operator \
+--project default \
+--repo https://github.com/testVinaycicd/learn-tf.git \
+--path learn-helm/precona-mongodb \
+--dest-server https://kubernetes.default.svc \
+--dest-namespace default \
+--sync-policy automated --self-heal --auto-prune --revision main \
+--kustomize-image "crysis307/mongodb=crysis307/mongodb:${image_tag}"
+
+
+# Cluster
