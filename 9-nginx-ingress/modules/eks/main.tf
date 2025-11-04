8aa8ce2a9d6065efcd24a88ee097827a600d928f
vinay <vinaybkrishna10@gmail.com>
2025-11-05 05:24:13 +0530

    terraform

--- 9-nginx-ingress/modules/eks/main.tf
+++ 9-nginx-ingress/modules/eks/main.tf
@@ -81,6 +81,14 @@ resource "aws_eks_access_entry" "main" {
   type              = "STANDARD"
 }
 
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name = var.cluster_name
+  addon_name   = "aws-ebs-csi-driver"
+  # addon_version = "v1.29.0-eksbuild.1" # optional pin
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+}
+
 resource "aws_eks_access_policy_association" "main" {
   for_each      = var.access
   cluster_name  = aws_eks_cluster.this.name
