91210acd8667fbb72049934e45baf515aee4d84d
vinay <vinaybkrishna10@gmail.com>
2025-11-12 23:39:01 +0530

    terraform

--- learn-helm/precona-mongodb/run.sh
+++ learn-helm/precona-mongodb/run.sh
@@ -12,7 +12,10 @@ argocd app create psmdb-operator \
 --dest-server https://kubernetes.default.svc \
 --dest-namespace default \
 --sync-policy automated --self-heal --auto-prune --revision main \
-
+--helm-set image.repository=percona/percona-server-mongodb-operator \
+--helm-set image.tag=1.16.1 \
+--helm-set operatorImage.repository=percona/percona-server-mongodb-operator \
+--helm-set operatorImage.tag=1.16.1
 
 
 # Cluster
