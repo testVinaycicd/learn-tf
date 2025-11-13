06cdd18e72b753589e251a89aed7684c559d3c1a
vinay <vinaybkrishna10@gmail.com>
2025-11-13 17:28:47 +0530

    terraform

--- 9-nginx-ingress/modules/vpc/main.tf
+++ 9-nginx-ingress/modules/vpc/main.tf
@@ -84,9 +84,10 @@ resource "aws_nat_gateway" "main" {
   }
 }
 
+
 resource "aws_route" "ngw" {
   count                  = length(local.private_route_table_ids)
   route_table_id         = local.private_route_table_ids[count.index]
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.main[count.index].id
+  nat_gateway_id         = element(aws_nat_gateway.main.*.id, count.index)
 }
