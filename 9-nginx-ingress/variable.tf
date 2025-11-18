c96a7bcb354e2d65ec73c46c55d8f8d9e8886cc5
vinay <vinaybkrishna10@gmail.com>
2025-11-18 22:44:26 +0530

    terraform

--- 9-nginx-ingress/variable.tf
+++ 9-nginx-ingress/variable.tf
@@ -1,33 +1,43 @@
-variable "aws_region" { type = string }
+# variable "aws_region" { type = string }
+#
+# variable "vpc_cidr" {
+#   type = string
+#   default = "11.0.0.0/16"
+# }
+#
+# variable "azs" {
+#   type = list(string)
+#   default = ["us-east-1a","us-east-1b"]
+# }
+#
+# variable "name" {}
+#
+# variable "access" {}
+#
+# variable "addons" {}
+#
+#
+# variable "vault_token" {
+#   sensitive = true
+#
+# }
+#
+# variable "env"{}
+# variable "bastion_ssh_nodes" {}
+# variable "db_instances"{}
+# variable "zone_id" {}
+# variable "kms_arn"{}
+#
+# variable "vpc" {}
+# variable "default_vpc" {}
+# variable "eks" {}
 
-variable "vpc_cidr" {
-  type = string
-  default = "11.0.0.0/16"
-}
-
-variable "azs" {
-  type = list(string)
-  default = ["us-east-1a","us-east-1b"]
-}
-
-variable "name" {}
-
-variable "access" {}
-
-variable "addons" {}
-
-
-variable "vault_token" {
-  sensitive = true
-
-}
-
-variable "env"{}
-variable "bastion_ssh_nodes" {}
-variable "db_instances"{}
+variable "db_instances" {}
 variable "zone_id" {}
-variable "kms_arn"{}
-
+variable "env" {}
+variable "vault_token" {}
+variable "eks" {}
 variable "vpc" {}
 variable "default_vpc" {}
-variable "eks" {}
+variable "bastion_ssh_nodes" {}
+variable "kms_arn" {}
