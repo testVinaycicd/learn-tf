4a79acd5885680d1d088e40e7faf68a3727fcc8d
vinay <vinaybkrishna10@gmail.com>
2025-11-19 18:56:45 +0530

    terraform

--- 12-vault/terraform/main.tf
+++ 12-vault/terraform/main.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "alb" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [local.my_ip_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
@@ -25,7 +25,7 @@ resource "aws_security_group" "alb" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
-    cidr_blocks = [local.my_ip_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
 
