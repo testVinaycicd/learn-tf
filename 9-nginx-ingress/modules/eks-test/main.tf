575b178b830d8a4f4105b4a4a82d1d54a0a5e615
vinay <vinaybkrishna10@gmail.com>
2025-11-20 02:08:37 +0530

    terraform

--- 9-nginx-ingress/modules/eks-test/main.tf
+++ 9-nginx-ingress/modules/eks-test/main.tf
@@ -57,12 +57,19 @@ resource "aws_eks_cluster" "this" {
   role_arn = aws_iam_role.eks_cluster.arn
   version  = var.eks_version
 
+  access_config {
+    authentication_mode                         = "API"
+    bootstrap_cluster_creator_admin_permissions = true
+  }
+
   vpc_config {
     subnet_ids = var.subnet_ids
     endpoint_private_access = false
     endpoint_public_access  = true
     public_access_cidrs     = ["0.0.0.0/0"]
   }
+
+
 
   tags = local.merged_tags
 
