7b5d48a91640efb4a6fd0fd37e0859cca3f77c34
vinay <vinaybkrishna10@gmail.com>
2025-11-19 14:23:40 +0530

    terraform

--- 9-nginx-ingress/modules/eks/main.tf
+++ 9-nginx-ingress/modules/eks/main.tf
@@ -22,48 +22,121 @@ resource "aws_eks_cluster" "main" {
   # }
 }
 
-resource "aws_launch_template" "main" {
+####working
+# resource "aws_launch_template" "main" {
+#   for_each = var.node_groups
+#   name     = each.key
+#
+#   block_device_mappings {
+#     device_name = "/dev/xvda"
+#
+#     ebs {
+#       volume_size = 20
+#       # encrypted   = true
+#       # # kms_key_id  = var.kms_arn
+#       #
+#       # kms_key_id  = aws_kms_key.eks_nodes.arn
+#     }
+#   }
+#
+# }
+#
+# resource "aws_eks_node_group" "main" {
+#   for_each        = var.node_groups
+#   cluster_name    = aws_eks_cluster.main.name
+#   node_group_name = each.key
+#   node_role_arn   = aws_iam_role.node-role.arn
+#   subnet_ids      = var.subnet_ids
+#   instance_types  = each.value["instance_types"]
+#   capacity_type   = each.value["capacity_type"]
+#
+#   launch_template {
+#     name    = aws_launch_template.main[each.key].name
+#     version = "$Latest"
+#   }
+#
+#   scaling_config {
+#     desired_size = each.value["min_nodes"]
+#     max_size     = each.value["max_nodes"]
+#     min_size     = each.value["min_nodes"]
+#   }
+#
+#   lifecycle {
+#     ignore_changes = [scaling_config]
+#   }
+#
+# }
+
+# 1. Tiny launch template – ONLY for custom KMS + bigger root volume
+resource "aws_launch_template" "kms_only" {
   for_each = var.node_groups
-  name     = each.key
+
+  name                   = "${var.env}-${each.key}-kms"
+  update_default_version = true                              # ← crucial
 
   block_device_mappings {
     device_name = "/dev/xvda"
-
     ebs {
-      volume_size = 20
-      # encrypted   = true
-      # # kms_key_id  = var.kms_arn
-      #
-      # kms_key_id  = aws_kms_key.eks_nodes.arn
+      volume_size           = 30           # or 50, whatever you want >20
+      encrypted             = true
+      kms_key_id            = var.kms_arn
+      delete_on_termination = true
     }
   }
 
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  # optional but nice
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "${var.env}-${each.key}"
+    }
+  }
 }
 
+# 2. Node group – NO disk_size, use id + $Latest
 resource "aws_eks_node_group" "main" {
   for_each        = var.node_groups
+  # in your case only "main"
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = each.key
   node_role_arn   = aws_iam_role.node-role.arn
   subnet_ids      = var.subnet_ids
-  instance_types  = each.value["instance_types"]
-  capacity_type   = each.value["capacity_type"]
+
+  capacity_type  = each.value["capacity_type"]   # SPOT
+  instance_types = each.value["instance_types"]
 
   launch_template {
-    name    = aws_launch_template.main[each.key].name
-    version = "$Latest"
+    id      = aws_launch_template.kms_only[each.key].id
+    version = "$Latest"                          # ← dollar sign is required
   }
 
   scaling_config {
     desired_size = each.value["min_nodes"]
-    max_size     = each.value["max_nodes"]
+    # 1
     min_size     = each.value["min_nodes"]
+    max_size     = each.value["max_nodes"]      # 10
+  }
+
+  # Very important when using SPOT + custom LT
+  update_config {
+    max_unavailable_percentage = 50
   }
 
   lifecycle {
     ignore_changes = [scaling_config]
   }
 
+  depends_on = [
+    aws_iam_role_policy_attachment.node-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.node-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.node-AmazonEC2ContainerRegistryReadOnly,
+  ]
 }
 
 
