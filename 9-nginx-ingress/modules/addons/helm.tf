3882c2dcd31919571ccbf255f44674bdd4759b56
vinay <vinaybkrishna10@gmail.com>
2025-12-02 13:53:03 +0530

    terraform

--- 9-nginx-ingress/modules/addons/helm.tf
+++ 9-nginx-ingress/modules/addons/helm.tf
@@ -322,99 +322,99 @@ resource "helm_release" "kube-prometheus-stack" {
 #
 #
 # }
-resource "helm_release" "cluster-autoscaler" {
+# resource "helm_release" "cluster-autoscaler" {
+#
+#   depends_on = [null_resource.kubeconfig]
+#   name       = "cluster-autoscaler"
+#   repository = "https://kubernetes.github.io/autoscaler"
+#   chart      = "cluster-autoscaler"
+#   namespace  = "kube-system"
+#   wait       = "false"
+#
+#   set = [{
+#     name  = "autoDiscovery.clusterName"
+#     value = var.cluster_name
+#   },
+#   {
+#     name  = "awsRegion"
+#     value = "us-east-1"
+#   }]
+# }
 
-  depends_on = [null_resource.kubeconfig]
-  name       = "cluster-autoscaler"
-  repository = "https://kubernetes.github.io/autoscaler"
-  chart      = "cluster-autoscaler"
-  namespace  = "kube-system"
-  wait       = "false"
-
-  set = [{
-    name  = "autoDiscovery.clusterName"
-    value = var.cluster_name
-  },
-  {
-    name  = "awsRegion"
-    value = "us-east-1"
-  }]
-}
-
-resource "helm_release" "wave-config-reloader" {
-
-  depends_on = [null_resource.kubeconfig]
-  name       = "wave"
-  repository = "https://wave-k8s.github.io/wave/"
-  chart      = "wave"
-  namespace  = "kube-system"
-  wait       = "false"
-
-  set=[ {
-    name  = "webhooks.enabled"
-    value = true
-  }]
-}
-
-resource "helm_release" "istio-base" {
-  depends_on = [
-    null_resource.kubeconfig
-  ]
-
-  name             = "istio-base"
-  repository       = "https://istio-release.storage.googleapis.com/charts"
-  chart            = "base"
-  namespace        = "istio-system"
-  create_namespace = true
-}
-
-resource "helm_release" "istiod" {
-  depends_on = [
-    null_resource.kubeconfig,
-    helm_release.istio-base
-  ]
-
-  name             = "istiod"
-  repository       = "https://istio-release.storage.googleapis.com/charts"
-  chart            = "istiod"
-  namespace        = "istio-system"
-  create_namespace = true
-  version = "1.25"
-}
-
-resource "null_resource" "kiali" {
-  depends_on = [
-    null_resource.kubeconfig,
-    helm_release.istiod
-  ]
-  provisioner "local-exec" {
-    command = <<EOF
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.26/samples/addons/kiali.yaml
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.26/samples/addons/prometheus.yaml
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.26/samples/addons/grafana.yaml
-kubectl apply -f - <<EOK
-apiVersion: networking.k8s.io/v1
-kind: Ingress
-metadata:
-  annotations:
-    nginx.ingress.kubernetes.io/backend-protocol: HTTP
-    nginx.ingress.kubernetes.io/secure-backends: "false"
-  name: kiali
-  namespace: istio-system
-spec:
-  ingressClassName: nginx
-  rules:
-  - host: kiali-dev.mikeydevops1.online
-    http:
-      paths:
-      - backend:
-          service:
-            name: kiali
-            port:
-              number: 20001
-        path: /kiali
-        pathType: Prefix
-EOK
-EOF
-  }
-}
+# resource "helm_release" "wave-config-reloader" {
+#
+#   depends_on = [null_resource.kubeconfig]
+#   name       = "wave"
+#   repository = "https://wave-k8s.github.io/wave/"
+#   chart      = "wave"
+#   namespace  = "kube-system"
+#   wait       = "false"
+#
+#   set=[ {
+#     name  = "webhooks.enabled"
+#     value = true
+#   }]
+# }
+#
+# resource "helm_release" "istio-base" {
+#   depends_on = [
+#     null_resource.kubeconfig
+#   ]
+#
+#   name             = "istio-base"
+#   repository       = "https://istio-release.storage.googleapis.com/charts"
+#   chart            = "base"
+#   namespace        = "istio-system"
+#   create_namespace = true
+# }
+#
+# resource "helm_release" "istiod" {
+#   depends_on = [
+#     null_resource.kubeconfig,
+#     helm_release.istio-base
+#   ]
+#
+#   name             = "istiod"
+#   repository       = "https://istio-release.storage.googleapis.com/charts"
+#   chart            = "istiod"
+#   namespace        = "istio-system"
+#   create_namespace = true
+#   version = "1.25"
+# }
+#
+# resource "null_resource" "kiali" {
+#   depends_on = [
+#     null_resource.kubeconfig,
+#     helm_release.istiod
+#   ]
+#   provisioner "local-exec" {
+#     command = <<EOF
+# kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.26/samples/addons/kiali.yaml
+# kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.26/samples/addons/prometheus.yaml
+# kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.26/samples/addons/grafana.yaml
+# kubectl apply -f - <<EOK
+# apiVersion: networking.k8s.io/v1
+# kind: Ingress
+# metadata:
+#   annotations:
+#     nginx.ingress.kubernetes.io/backend-protocol: HTTP
+#     nginx.ingress.kubernetes.io/secure-backends: "false"
+#   name: kiali
+#   namespace: istio-system
+# spec:
+#   ingressClassName: nginx
+#   rules:
+#   - host: kiali-dev.mikeydevops1.online
+#     http:
+#       paths:
+#       - backend:
+#           service:
+#             name: kiali
+#             port:
+#               number: 20001
+#         path: /kiali
+#         pathType: Prefix
+# EOK
+# EOF
+#   }
+# }
